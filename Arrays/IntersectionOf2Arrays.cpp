f510915d7c4b6093eb2dd7c864baca27e2952d34
Ayushmann Choudhary <ayushmaanchoudhary12@gmail.com>
2026-07-11 20:51:42 +0530

    Update

---
+++ Arrays/IntersectionOf2Arrays.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n1, n2;
+    cout<<"Enter the size of first array : "<<endl;
+    cin>>n1;
+    int arr1[n1];
+    cout<<"Enter the elements of an array : "<<endl;
+    for(int i = 0; i<n1; i++){
+        cin>>arr1[i];
+    }
+    cout<<"Enter the size of second array : "<<endl;
+    cin>>n2;
+    int arr2[n2];
+    cout<<"Enter the elements of an array : "<<endl;
+    for(int j = 0; j<n2; j++){
+        cin>>arr2[j];
+    }
+    int i = 0, j = 0;
+    vector<int> ans;
+    while(i < n1 && j < n2){
+        if(arr1[i] < arr2[j]){
+            i++;
+        }
+        else if(arr1[i] > arr2[j]){
+            j++;
+        }
+        else{
+            ans.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+    }
+    cout<<"Intersection of two arrays is : "<<endl;
+    for(int x : ans){
+        cout<<x<<" ";
+    }
+    return 0;
+}
