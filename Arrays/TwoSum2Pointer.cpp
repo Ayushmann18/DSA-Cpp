9245b74d589b9d141d90d54eb4587f7a5597cd77
Ayushmann Choudhary <ayushmaanchoudhary12@gmail.com>
2026-07-13 00:24:37 +0530

    Update

---
+++ Arrays/TwoSum2Pointer.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n; 
+    cout<<"Enter size of an array : "<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<"Enter sorted array : "<<endl;
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+    int target;
+    cout<<"Enter target : "<<endl;
+    cin>>target;
+    int start = 0;
+    int end = n-1;
+    while(start < end){
+        int sum = arr[start] + arr[end];
+        if(sum == target){
+            cout<<"Pair found : "<<start<<" "<<end<<endl;//returns index number of the pair
+            return 0;
+        }
+        else if(sum < target){
+            start++;
+        }
+        else{
+            end--;
+        }
+    }
+    cout<<"No pair found!"<<endl;
+    return 0;
+}
---
+++ Arrays/TwoSumBF.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int target;
+    cout << "Enter target: ";
+    cin >> target;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] + arr[j] == target)
+            {
+                cout << "Indices: " << i << " " << j;
+                return 0;
+            }
+        }
+    }
+
+    cout << "No pair found";
+
+    return 0;
+}
