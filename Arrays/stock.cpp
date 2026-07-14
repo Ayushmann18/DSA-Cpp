a76cb2199fd9ae5df12abd97eb40708a71737a9d
Ayushmann Choudhary <ayushmaanchoudhary12@gmail.com>
2026-07-14 19:16:43 +0530

    Update

---
+++ Arrays/stock.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter size of an array : "<<endl;
+    cin >> n;
+    int price[n];
+    cout<<"Enter elements of array : "<<endl;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> price[i];
+    }
+    int maxProfit = 0;
+    int bestBuy = price[0];
+    for(int i = 1; i < n; i++)
+    {
+        if(price[i] > bestBuy)
+        {
+            maxProfit = max(maxProfit, price[i] - bestBuy);
+        }
+        bestBuy = min(bestBuy, price[i]);
+    }
+    cout << "maximum profit is : "<<maxProfit<<endl;
+    return 0;
+}
