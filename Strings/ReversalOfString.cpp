2f636e2cb649d6bd2d5d12f8f6d8d5c3d179c6e2
Ayushmann Choudhary <ayushmaanchoudhary12@gmail.com>
2026-07-18 10:32:53 +0530

    Save progress

---
+++ Strings/ReversalOfString.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+//#include<string>
+using namespace std;
+int main() {
+    string str = "Ayushmann!";
+    reverse(str.begin(), str.end());
+    cout<< str <<endl;
+    return 0;
+}
