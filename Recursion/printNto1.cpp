12ded3ad170d7cc7ba1dc9c460a0efeb83c059a7
Ayushmann18 <ayushmaanchoudhary12gmail.com@gmail.com>
2026-08-29 17:44:42 +0530

    Save

---
+++ Recursion/printNto1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printNums(int n){
+    if(n==1){
+        cout<<"1\n";
+        return;
+    }
+    cout<<n<<" ";
+    printNums(n-1);
+
+}
+int main() {
+    printNums(10);
+    return 0;
+}
