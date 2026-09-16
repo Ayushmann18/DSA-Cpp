45833b0e0be2891b13f27837e44718e1b962d275
Ayushmann18 <ayushmaanchoudhary12gmail.com@gmail.com>
2026-09-17 01:59:39 +0530

    Save

--- Recursion/checkSortedArray.cpp
+++ Recursion/checkSortedArray.cpp
@@ -4,7 +4,7 @@ bool isSorted(int arr[], int n){
     if(n == 0 || n == 1){
         return true;
     }
-    return arr[n-1] <= arr[n] && isSorted(arr, n-1);
+    return arr[n-1] >= arr[n - 2] && isSorted(arr, n-1);
 }
 int main() {
     int arr[] = {1, 2, 3, 4, 5};
